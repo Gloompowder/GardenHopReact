245a0af6ed6a14ddb6b8ec3e338638753a733d59
William <willlin1996@gmail.com>
2020-11-25 18:32:25 -0500

    save

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Search from '../src/components/Search'
 import GardenCard from '../src/containers/GardenDiv'
 import Home from '../src/components/Home'
 import ProfileDiv from '../src/components/ProfileDiv'
+import Test from '../src/components/Test'
 
 import {
   BrowserRouter as Router,
@@ -14,12 +15,23 @@ import {
   Route
 } from "react-router-dom";
 
+const [searches, setSearches] = useState("")
+const searching = (e) => {
+    // taking the data and getting each object before calling setgardens
+    // data.props.garden
+    // setGardens(data.props.garden)
+    useEffect((e)=>{
+      setSearches(e.target.value)
+    })
+}
+
 class App extends React.Component{
   render(){
       return(
       <div className="garden">
         <Nav/>
-        <Search/>
+        <Search searching = {searching}/>
+        <Test/>
             <Router>
               <Switch>
                 <Route path="/gardens/:id" component={GardenCard}/>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 function Search(props){
+    // const [mapToggled, setmapToggled] = useState(false)
+
+    // const togglingMap=()=>{
+    //     if (mapToggled) {
+    //         setmapToggled(false)
+    //     }
+    //     else {
+    //         setmapToggled(true)
+    //     }
+    // }
+    // const [search, setsearch] = useState("")
+    
+    // const searching=(e)=>{
+    //     setsearch(e.target.value)
+    // }
+    console.log(props.searching)
     return(
-        <div className="search" >
-        </div>
+            <form className="search">
+                <input type="text" />
+                <button className="search-button">Search</button>
+            </form>
     )
 }
 
